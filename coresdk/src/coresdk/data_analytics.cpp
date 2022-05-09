6c15af17ce017f4a6a398ab6b456d62e46a5df8c
Ryan <ryan.lawrence.business@gmail.com>
2022-05-09 14:48:13 +1000

    refactor: simplify data validation methods

---
+++ coresdk/src/coresdk/data_analytics.cpp
@@ -0,0 +1,197 @@
+#include "data_analytics.h"
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <sstream>
+#include <string>
+#include <regex>
+#include "terminal.h"
+#include "basics.h"
+
+using namespace std;
+
+// REGEX PATTERNS
+const regex symbols_pattern("^(?=.*[-+_!@#$%^&*., ?]).+$");
+const regex ABC_pattern("^(?=.*[A-z]).+$");
+const regex URL_pattern("^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$");
+const regex phone_number_pattern("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$");
+const regex email_pattern("[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}");
+const regex time_pattern("([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])");
+
+
+
+
+namespace splashkit_lib
+{
+    // Restrict decimal places to 3             [POSSIBLY REDUNDANT, C++ HAS ROUND FEATURE]
+    string set_decimal_places(string s)
+    {
+        string temp;
+        stringstream ss;
+
+        temp = s.substr(s.find(".") + 1);
+
+        if (temp.length() > 3)
+        {
+            write_line("Restricting to 3 decimals...");
+
+            double value = convert_to_double(s);
+
+            ss << fixed;
+            ss << setprecision(3);
+            ss << value;
+
+            s = ss.str();
+
+            write_line(s);
+        }
+        return s;
+    }
+
+    // Get custom filename                              [[[TO EXAMINE]]]
+    string get_file_name(string original_file_path)
+    {
+        __fs::filesystem::path p(original_file_path);
+
+        // Let user choose to keep or rename file
+        write("Do you want to keep the file name? (y/n) ");
+        string keep_file_name = read_line();
+        string file_name;
+
+        // Keep file name
+        if (keep_file_name == "y")
+        {
+            file_name = p.filename();
+        }
+        // New file name
+        else if (keep_file_name == "n")
+        {
+            write("New file name: ");
+            file_name = read_line();
+            file_name += p.extension();
+        }
+        // Invalid input
+        else
+        {
+            write_line("Invalid input");
+        }
+
+        return file_name;
+    }
+
+    // Save data into current file
+    void save_file(string original_file_path, string data)
+    {
+        ofstream file(original_file_path);
+
+        if (file.is_open())
+        {
+            file << data;
+            file.close();
+        }
+        else
+            cout << "Unable to save file";
+    }
+
+
+    // <<<< COMMENTED OUT FOR RELIANCE ON PORTABLE FILE DIALOGS >>>>
+
+    // // Save file at different location and custom name
+    // void save_file_as(string original_file_path, string data)
+    // {
+    //     write_line("Choose a destination folder to save file:");
+
+    //     // Open selection dialog
+    //     auto destination_path = pfd::select_folder("Select a folder").result();
+    //     if (!destination_path.empty())
+    //         cout << "User selected folder " << destination_path << "\n";
+
+    //     string file_name = get_file_name(original_file_path);
+
+    //     string destination_file = destination_path + "/" + file_name;
+
+    //     ofstream file(destination_file);
+
+    //     if (file.is_open())
+    //     {
+    //         file << data;
+    //         file.close();
+    //     }
+    //     else
+    //         cout << "Unable to save file";
+    // }
+
+    bool input_has_symbol(string input)
+    {
+        return regex_match(input, symbols_pattern);
+    }
+
+    bool input_has_char(string input)
+    {
+        return regex_match(input, ABC_pattern);
+    }
+
+    bool input_is_valid_url(string input) {
+        return regex_match(input, URL_pattern);
+    }
+
+    bool input_is_email(string input) {
+        return regex_match(input, email_pattern);
+    }
+
+    bool input_is_phone_number(string& input) {
+        return regex_match(input, phone_number_pattern);
+    }
+
+    bool input_is_time(string input) {
+        return regex_match(input, time_pattern);
+    }
+
+    // Provides a set of options for text validation. Returns input type.
+    std::string text_validation(string input)
+    {
+        // Output menu with validation option
+        write_line('== Text Validation ===\n1. Blank\n2. Email \n3. Phone number\n4. All\nSelect an option: ');
+
+        int choice = convert_to_integer(read_line());
+
+        // Switch validation option to cases
+        switch (choice)
+        {
+        case 1:
+            write_line('== Validating blank input ==');
+            if (input.empty())
+            {
+                return to_string('blank');
+            } 
+            break;
+
+        case 2:
+            write_line('== Validating email ==');
+            if (input_is_email(input))
+            {
+                return to_string('email');
+            }
+            break;
+
+        case 3:
+            write_line('== Validating phone number ==');
+            if (input_is_phone_number)
+            {
+                return to_string('phone');
+            }
+            break;
+
+        case 4:
+            write_line('== Validating text with validation set ==');
+            if (input_is_phone_number(input))
+            {
+                
+            }
+         
+        default:
+            write_line('Your option is not valid! Please try again!');
+            return to_string('INVALID');
+        }
+    }
+}
---
+++ coresdk/src/coresdk/data_analytics.h
@@ -0,0 +1,150 @@
+#ifndef data_analytics_h
+#define data_analytics_h
+
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+string set_decimal_places(string input);
+
+string get_file_name(string original_file_path);
+
+void save_file(string input, string path);
+
+void save_file_as(string input);
+
+bool input_has_symbol(string input);
+
+bool input_has_char(string input);
+
+bool input_is_valid_url(string input);
+
+bool input_is_empty(string input);
+
+bool input_is_email(string input);
+
+bool input_is_phone_number(string& input);
+
+bool input_is_time(string input);
+
+string text_validation(string input);
+
+//DataFrames
+/*
+DataFrame stores tables of data which can be used by other modules.
+Can store information to be manipulated, selected, sorted, exported
+Has 6 constructors and cannot be created with an Empty Constructor
+Currently Does not accept Char
+*/
+
+template<typename T>
+class DataFrame{
+    public:
+    enum flags{
+        OVERRIDE_FILE = 0x01,
+        APPEND_FILE = 0x02,
+        ASCENDING = 0x04,
+        DESCENDING = 0x08
+    };
+    //Basic Constructor/Destructor
+    DataFrame() = delete;
+    ~DataFrame();
+    
+    //Takes DataFrame as R-Value (Allows std::move)
+    DataFrame(const DataFrame&& _other);
+    //Takes DataFrame as L-Value (Allows copy)
+    DataFrame(DataFrame& _other);
+    //Import From TextFile
+    DataFrame(string _textFileName, bool _index);
+    //Create using Array or Vector
+    DataFrame(unsigned int _columns, const string (&_tempHeader)[], bool _index);
+    DataFrame(unsigned int _columns, const string (_tempHeader)[], bool _index);
+    DataFrame(vector<string> _tempHeader, bool _index);    
+
+    //Export To FILE
+    const void exportToFile(string _fileName, flags _eType = OVERRIDE_FILE) const;
+
+    //Push back new row into dataframe
+    void push_back(const string (&_arr)[], unsigned int _length);
+    void push_back(const string (_arr)[], unsigned int _length);
+    void push_back(const DataFrame<T> _dFrame);
+
+    //Insert new row into DataFrame
+    void insert(const string (&_arr)[], unsigned int _length, unsigned int _pos);
+    void insert(const string (_arr)[], unsigned int _length, unsigned int _pos);
+    void insert(const DataFrame<T> _dFrame, unsigned int _pos);
+
+    //Insert Column
+
+    //Insert with Default Number
+    void insertC(const string headerName, string defaultValue = "0");
+    //void insertC(const string headerName, const string (&_arr)[]);
+    //void insertC(const string headerName, const string (_arr)[]);
+
+    //Delete Row at index
+    void deleteRow(unsigned int _row);
+    void deleteColumn(unsigned int _column);
+
+    
+    // Indexing Function
+    //Return Row
+    const DataFrame<T> iLoc(int _row) const { return getRow(_row);};
+
+    //Return Cell
+    T& iLoc(unsigned int _row, unsigned int _column) { return m_Table[(_row + (_column * m_Columns))];};
+    const T iLoc  (unsigned int _row, unsigned int _column) const { return m_Table[(_row + (_column * m_Columns))] ;};
+
+    //Return Column
+    const DataFrame<T> Loc(string _column) const {return getColumn(_column); };
+
+    //Get Row
+    const DataFrame<T> getRow (unsigned int _row)  const;
+
+    //Get Column
+    const DataFrame<T> getColumn(unsigned int _column) const;
+    const DataFrame<T> getColumn(string _column) const;
+    
+    //Possibly Use >> to signify Range. Create Function Capability using members
+    
+    //Swap
+    void swap(unsigned int _index1, unsigned int _index2);
+
+    //Sort
+    void sort(int _column, flags _flag);
+
+    //Moving Function
+    void transpose();
+    
+    //Display Dataframe
+    const void display() const;
+    const void fullDisplay() const;
+
+    //ResetIndex
+    void resetIndex();
+
+    //Size, Dimensions and ReSize
+    const unsigned int size() const { return m_Columns * m_Rows;};
+    const string dimensions() const { std::ostringstream os; os << m_Rows << " X " << m_Columns; return os.str();};
+    const void reserve(unsigned int _amount) const { m_Table.reserve(_amount);};
+
+
+
+    private:
+    unsigned int m_Columns = 0;
+    unsigned int m_Rows = 0;
+    bool m_Index = true;
+    /*Storing as a single table 
+    By Storing in a single table the memory doesn't fragment,
+    the process runs faster but also requires more care especially when adding columns
+    This can be optimised in the future */
+    vector<string> m_HeaderNames;
+    vector<T> m_Table;
+    vector<string> m_IndexNames;
+    int sMod(int a, int b) {return (a % b + b) % b;};
+    double partition(int _low, int _high, int _column, flags _flag);
+    void quickSort(int _low, int _high, int _column, flags _flag);
+
+};
+
+#endif
