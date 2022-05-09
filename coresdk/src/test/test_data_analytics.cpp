992bb1747b13e869f7150ff32188b94a5f7101b5
timmydzeko <tdmoo@deakin.edu.au>
2022-05-09 16:12:27 +1000

    Create testing file for data analytics module with Hello World Program

---
+++ coresdk/src/test/test_data_analytics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "input.h"
+#include "data_analytics.h"
+#include "terminal.h"
+
+using namespace splashkit_lib;
+
+void run_data_analytics_test()
+{
+    write_line("Hello World!");
+}
