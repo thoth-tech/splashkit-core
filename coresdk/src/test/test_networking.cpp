63e26d5147301ee87da27c11bdae2aa451e6e305
Breezy <114720008+breezy-codes@users.noreply.github.com>
2024-09-07 17:16:40 +1000

    network function tests

--- coresdk/src/test/test_networking.cpp
+++ coresdk/src/test/test_networking.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 #define TEST_IP "127.0.0.1"
-#define TEST_IP_HEX "0x7F000001"
+#define TEST_IP_DEC 2130706433  // Decimal representation of 127.0.0.1
 
 using namespace splashkit_lib;
 
@@ -14,22 +14,41 @@ void run_encoding_decoding_tests()
 {
     // Incomplete ip address tests
     assert(ipv4_to_hex("127.0.0") == "0x7F000000");
-    assert(ipv4_to_str(ipv4_to_dec("127.0.0")) == "127.0.0.0");
-    assert(ipv4_to_str(ipv4_to_dec("127,0.0")) == "127.0.0.0");
-    assert(ipv4_to_str(ipv4_to_dec("")) == "0.0.0.0");
+    assert(dec_to_ipv4(ipv4_to_dec("127.0.0")) == "127.0.0.0");
+    assert(dec_to_ipv4(ipv4_to_dec("127,0.0")) == "127.0.0.0");
+    assert(dec_to_ipv4(ipv4_to_dec("")) == "0.0.0.0");
 
-    int too_small_int = 4000;
-    assert(ipv4_to_str(too_small_int) == "0.0.15.160");
 
     // Complete ip address tests
-    assert(ipv4_to_str(ipv4_to_dec(TEST_IP)) == TEST_IP);
-    assert(dec_to_hex(ipv4_to_dec(TEST_IP)) == TEST_IP_HEX);
-    assert(ipv4_to_hex(TEST_IP) == TEST_IP_HEX);
-    assert(hex_str_to_ipv4(TEST_IP_HEX) == TEST_IP);
-    assert(my_ip() == "127.0.0.1");
+    assert(dec_to_ipv4(ipv4_to_dec(TEST_IP)) == TEST_IP);
+    assert(dec_to_hex(ipv4_to_dec(TEST_IP)) == "0x7F000001");
+    assert(ipv4_to_hex(TEST_IP) == "0x7F000001");
+    assert(hex_str_to_ipv4("0x7F000001") == TEST_IP);
+    assert(my_ip() == TEST_IP);
 }
 
 void run_networking_test()
 {
     run_encoding_decoding_tests();
+    std::cout << "Starting decimal to IPv4 and IPv4 to decimal tests" << std::endl;
+
+    // Testing decimal to IPv4 conversion
+    int decimal_ip = TEST_IP_DEC;
+    std::string converted_ip = dec_to_ipv4(decimal_ip);
+    std::cout << "Decimal " << decimal_ip << " converted to IPv4: " << converted_ip << std::endl;
+    assert(converted_ip == TEST_IP);
+
+    // Testing IPv4 to decimal conversion
+    std::string ip_address = TEST_IP;
+    int converted_decimal = ipv4_to_dec(ip_address);
+    std::cout << "IPv4 " << ip_address << " converted to decimal: " << converted_decimal << std::endl;
+    assert(converted_decimal == decimal_ip);
+
+    std::string test_ip_2 = "10.0.0.1";
+    int dec_ip_2 = ipv4_to_dec(test_ip_2);
+    std::cout << "IPv4 " << test_ip_2 << " converted to decimal: " << dec_ip_2 << std::endl;
+
+    std::string result_ip_2 = dec_to_ipv4(dec_ip_2);
+    std::cout << "Decimal " << dec_ip_2 << " converted back to IPv4: " << result_ip_2 << std::endl;
+    assert(result_ip_2 == test_ip_2);
 }
