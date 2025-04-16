bd1fb079bee885b29d0733c2abb514a26b48cf02
omckeon <ordmckeon@gmail.com>
2025-04-16 16:02:55 +1000

    Add suffix attribute to some overloaded functions for translator

--- coresdk/src/coresdk/raspi_adc.h
+++ coresdk/src/coresdk/raspi_adc.h
@@ -14,30 +14,30 @@
 
 namespace splashkit_lib
 {
-    /**
-        * The `adc_device` type is used to refer to ADC (Analog-to-Digital Converter)
-        * devices that can be managed by the SplashKit ADC code. ADC devices are:
-        *
-        *   - loaded with `load_adc_device`,
-        *
-        *   - accessed using `adc_device_named` or checked with `has_adc_device`,
-        *
-        *   - read using `read_adc_channel` to retrieve analog values from specific channels,
-        *
-        *   - and must be released using `free_adc_device` (to release a specific
-        *     ADC device) or `free_all_adc_devices` (to release all loaded ADC devices).
-        *
-        * ADC devices allow you to interface with external analog sensors or inputs,
-        * converting their signals into digital values for processing in your application.
-        *
-        * You can check if an ADC device is loaded using `has_adc_device`.
-        *
-        * Use `free_adc_device` to release resources associated with a specific ADC device,
-        * or `free_all_adc_devices` to clean up all ADC devices.
-        *
-        * @attribute class adc_device
-        */
     // Define the ADC device type using a pointer to an internal structure
+    /**
+     * The `adc_device` type is used to refer to ADC (Analog-to-Digital Converter)
+     * devices that can be managed by the SplashKit ADC code. ADC devices are:
+     *
+     *   - loaded with `load_adc_device`,
+     *
+     *   - accessed using `adc_device_named` or checked with `has_adc_device`,
+     *
+     *   - read using `read_adc_channel` to retrieve analog values from specific channels,
+     *
+     *   - and must be released using `free_adc_device` (to release a specific
+     *     ADC device) or `free_all_adc_devices` (to release all loaded ADC devices).
+     *
+     * ADC devices allow you to interface with external analog sensors or inputs,
+     * converting their signals into digital values for processing in your application.
+     *
+     * You can check if an ADC device is loaded using `has_adc_device`.
+     *
+     * Use `free_adc_device` to release resources associated with a specific ADC device,
+     * or `free_all_adc_devices` to clean up all ADC devices.
+     *
+     * @attribute class adc_device
+     */
     typedef struct _adc_data *adc_device;
 
     /**
@@ -46,7 +46,7 @@ namespace splashkit_lib
      * @param name The name used to identify the ADC device.
      * @returns true if an ADC device with the supplied name exists.
      */
-    bool has_adc_device(const std::string &name);
+    bool has_adc_device(const string &name);
 
     /**
      * Retrieve an ADC device that has been loaded.
@@ -54,7 +54,7 @@ namespace splashkit_lib
      * @param name The name of the ADC device.
      * @returns The adc_device pointer if found; otherwise, nullptr.
      */
-    adc_device adc_device_named(const std::string &name);
+    adc_device adc_device_named(const string &name);
 
     /**
      * Loads an ADC device on the specified I2C bus at a given address.
@@ -66,6 +66,7 @@ namespace splashkit_lib
      * @returns A valid adc_device on success, or nullptr on failure.
      */
     adc_device open_adc(const string &name, int bus, int address, adc_type type);
+
     /**
      * Opens an ADC device with the specified name and type. Defaults to bus 1 and address 0x48.
      *
@@ -74,7 +75,7 @@ namespace splashkit_lib
      * @returns A valid adc_device on success, or nullptr on failure.
      */
     adc_device open_adc(const string &name, adc_type type);
-    
+
     /**
      * @brief Reads an 8-bit value from the specified ADC channel on the device.
      *
@@ -94,8 +95,11 @@ namespace splashkit_lib
      * @param adc  The ADC name string to close.
      * @param channel  The channel number to read (range depends on ADC type).
      * @returns        The ADC conversion value (0–255), or -1 on error.
+     *
+     * @attribute suffix  with_name
      */
     int adc_read(const string &name, adc_pin channel);
+
     /**
      * @brief Closes an ADC device given its pointer.
      *
@@ -111,6 +115,8 @@ namespace splashkit_lib
      * This function closes an ADC device given its name.
      *
      * @param name  The name of the ADC device to close.
+     *
+     * @attribute suffix  with_name
      */
     void close_adc(const string &name);
 
