e7a7e8ed5de206bad35f5abf841690c7fc494c65
DarrenSunandar <94236101+DarrenSunandar@users.noreply.github.com>
2025-07-04 14:40:29 +1000

    Create Python PIP Package (#65)
    * Create Python PIP Package
    
    * Fix issues

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+import os
+
+splashkit_module = Extension(
+    name="splashkit",
+    sources=[os.path.join("languages", "cpp", "splashkit_starter.cpp")],
+    include_dirs=[os.path.join("languages", "cpp")],
+    language="c++",
+)
+
+setup(
+    name="splashkit",
+    version="0.1.0",
+    description="Python bindings for the SplashKit library",
+    ext_modules=[splashkit_module],
+)
