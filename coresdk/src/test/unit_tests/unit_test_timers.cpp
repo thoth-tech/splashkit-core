27049d5da10268674a2f2a35adf5bcede4c8b255
stelaukin <stealukin@outlook.com>
2026-01-06 13:39:06 +1100

    update timer test

--- coresdk/src/test/unit_tests/unit_test_timers.cpp
+++ coresdk/src/test/unit_tests/unit_test_timers.cpp
@@ -10,10 +10,23 @@
 
 using namespace splashkit_lib;
 
-TEST_CASE("timmy timer", "[timers]")
+TEST_CASE("test create and free timer", "[timers][free_timer][create_timer]")
 {
     timer t = create_timer("test_timer");
     REQUIRE(has_timer("test_timer"));
     free_timer(t);
     REQUIRE_FALSE(has_timer("test_timer"));
+}
+
+TEST_CASE("pause", "[timers][free_timer][create_timer]")
+{
+    timer t2 = create_timer("test_pause");
+    start_timer(t2);
+    while (timer_ticks(t2) <= 0)
+    {
+    }
+    REQUIRE(timer_ticks(t2) > 0);
+    pause_timer(t2);
+    unsigned int ticks = timer_ticks(t2);
+    REQUIRE(timer_ticks(t2) == ticks);
 }
