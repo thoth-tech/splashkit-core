9a362335a5959c0dff08858cbf7812f2ce9c5acc
stelaukin <stealukin@outlook.com>
2026-01-11 12:54:10 +1100

    start pause timer test

--- coresdk/src/test/unit_tests/unit_test_timers.cpp
+++ coresdk/src/test/unit_tests/unit_test_timers.cpp
@@ -3,30 +3,33 @@
  */
 
 #include "catch.hpp"
-
 #include "timers.h"
-
 #include "logging_handling.h"
 
 using namespace splashkit_lib;
 
-TEST_CASE("test create and free timer", "[timers][free_timer][create_timer]")
+TEST_CASE("create_timer and free_timer", "[timers][create_timer][free_timer]")
 {
     timer t = create_timer("test_timer");
     REQUIRE(has_timer("test_timer"));
+
     free_timer(t);
     REQUIRE_FALSE(has_timer("test_timer"));
 }
 
-TEST_CASE("pause", "[timers][free_timer][create_timer]")
+TEST_CASE("start_timer and pause_timer", "[timers][start_timer][pause_timer]")
 {
-    timer t2 = create_timer("test_pause");
-    start_timer(t2);
-    while (timer_ticks(t2) <= 0)
+    timer t = create_timer("test_pause");
+    REQUIRE(timer_ticks(t) == 0); // starts at 0
+
+    start_timer(t);
+    while (timer_ticks(t) == 0)
     {
+        // wait until some ticks have passed
     }
-    REQUIRE(timer_ticks(t2) > 0);
-    pause_timer(t2);
-    unsigned int ticks = timer_ticks(t2);
-    REQUIRE(timer_ticks(t2) == ticks);
+    REQUIRE(timer_ticks(t) > 0); // time has advanced
+
+    pause_timer(t);
+    unsigned int ticks = timer_ticks(t);
+    REQUIRE(timer_ticks(t) == ticks); // ticks do not change while paused
 }
