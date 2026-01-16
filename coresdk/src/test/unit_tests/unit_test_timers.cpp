4102df5a48bfed1e2557de6a0deddf1a5464a9bd
stelaukin <stealukin@outlook.com>
2026-01-16 20:07:29 +1100

    changes made based on peer review

--- coresdk/src/test/unit_tests/unit_test_timers.cpp
+++ coresdk/src/test/unit_tests/unit_test_timers.cpp
@@ -8,28 +8,28 @@
 
 using namespace splashkit_lib;
 
-TEST_CASE("create_timer and free_timer", "[timers][create_timer][free_timer]")
+TEST_CASE(
+    "timer can be created, started, paused, and freed",
+    "[timers][create_timer][free_timer][start_timer][pause_timer]")
 {
-    timer t = create_timer("test_timer");
-    REQUIRE(has_timer("test_timer"));
+    timer t = create_timer("test");
+    REQUIRE(has_timer("test"));
 
-    free_timer(t);
-    REQUIRE_FALSE(has_timer("test_timer"));
-}
-
-TEST_CASE("start_timer and pause_timer", "[timers][start_timer][pause_timer]")
-{
-    timer t = create_timer("test_pause");
     REQUIRE(timer_ticks(t) == 0); // starts at 0
 
+    int attempts = 0;
     start_timer(t);
-    while (timer_ticks(t) == 0)
+    while (timer_ticks(t) == 0 || attempts < 1000)
     {
-        // wait until some ticks have passed
-    }
+        attempts++;
+    };
+
     REQUIRE(timer_ticks(t) > 0); // time has advanced
 
     pause_timer(t);
     unsigned int ticks = timer_ticks(t);
     REQUIRE(timer_ticks(t) == ticks); // ticks do not change while paused
+
+    free_timer(t);
+    REQUIRE_FALSE(has_timer("test")); // check the same name we created
 }
