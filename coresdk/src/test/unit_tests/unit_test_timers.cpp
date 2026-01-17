92f9b75938d4f1da9bc80ef13fd4a43d5e5b88c3
stelaukin <stealukin@outlook.com>
2026-01-17 17:33:11 +1100

    remove folder and invidivualise tests cases

--- coresdk/src/test/unit_tests/unit_test_timers.cpp
+++ coresdk/src/test/unit_tests/unit_test_timers.cpp
@@ -5,31 +5,65 @@
 #include "catch.hpp"
 #include "timers.h"
 #include "logging_handling.h"
+#include "utils.h"
 
 using namespace splashkit_lib;
 
 TEST_CASE(
-    "timer can be created, started, paused, and freed",
-    "[timers][create_timer][free_timer][start_timer][pause_timer]")
+    "timer can be created",
+    "[timers][create_timer]")
 {
     timer t = create_timer("test");
     REQUIRE(has_timer("test"));
+    free_timer(t);
+}
 
-    REQUIRE(timer_ticks(t) == 0); // starts at 0
+TEST_CASE(
+    "timer starts at zero ticks",
+    "[timers][create_timer][timer_ticks]")
+{
+    timer t = create_timer("test");
+    REQUIRE(timer_ticks(t) == 0);
+    free_timer(t);
+}
 
-    int attempts = 0;
+TEST_CASE(
+    "timer advances after start",
+    "[timers][start_timer][timer_ticks]")
+{
+    timer t = create_timer("test");
     start_timer(t);
-    while (timer_ticks(t) == 0 || attempts < 1000)
-    {
-        attempts++;
-    };
 
-    REQUIRE(timer_ticks(t) > 0); // time has advanced
+    delay(200);
+
+    REQUIRE(timer_ticks(t) > 0);
+
+    free_timer(t);
+}
+
+TEST_CASE(
+    "timer does not advance while paused",
+    "[timers][pause_timer][timer_ticks]")
+{
+    timer t = create_timer("test");
+    start_timer(t);
+    delay(200);
 
     pause_timer(t);
     unsigned int ticks = timer_ticks(t);
-    REQUIRE(timer_ticks(t) == ticks); // ticks do not change while paused
+    delay(200);
+
+    REQUIRE(timer_ticks(t) == ticks);
 
     free_timer(t);
-    REQUIRE_FALSE(has_timer("test")); // check the same name we created
+}
+
+TEST_CASE(
+    "timer can be freed",
+    "[timers][free_timer]")
+{
+    timer t = create_timer("test");
+    free_timer(t);
+
+    REQUIRE_FALSE(has_timer("test"));
 }
