bd24a5cfb62e3a328318cf4c60629d8c278a2bd9
Kiandisor <mojynardi@gmail.com>
2026-07-30 14:05:54 +1000

    Changes to bin_to_dec clamping
    Changed the way that clamping worked in the bin_to_dec function to
    convert the value first then check if the value is larger than unsigned
    int.
    
    Changed unit tests to reflect the changes

--- coresdk/src/coresdk/basics.cpp
+++ coresdk/src/coresdk/basics.cpp
@@ -205,16 +205,21 @@ namespace splashkit_lib
             LOG(ERROR) << "Invalid binary string \"" << bin_str << "\" passed to bin_to_dec. Returning 0.";
             return 0;
         }
-        if(bin_str.length() > 32)
-        {
-	  return std::numeric_limits<unsigned int>::max();
-        }
 
         try
         {
-	  return static_cast<unsigned int>(std::stoul(bin_str, nullptr, 2));
+	  const unsigned long result = std::stoul(bin_str, nullptr, 2);
+
+	  if (result > std::numeric_limits<unsigned int>::max())
+	  {
+	      return std::numeric_limits<unsigned int>::max();
+	  }
+	  else
+	  {
+	      return static_cast<unsigned int>(result);
+	  }
         }
-        catch(const std::exception& error)
+        catch (const std::exception& error)
         {
             LOG(ERROR) << "Invalid binary string \"" << bin_str << "\" passed to bin_to_dec. Returning 0.";
             return 0;
--- coresdk/src/test/test_terminal.cpp
+++ coresdk/src/test/test_terminal.cpp
@@ -125,6 +125,9 @@ void bin_to_dec()
     // High values (32-bit boundaries)
     assert(bin_to_dec("10000000000000000000000000000000") == 2147483648);
 
+    // Test for over unsigned int max
+    assert(bin_to_dec("1111111111111111111111111111111111111111111111111111111111111111") == std::numeric_limits<unsigned int>::max());
+
     // Mixed bits
     assert(bin_to_dec("1111011") == 123);
     assert(bin_to_dec("10000000001") == 1025);
@@ -144,11 +147,6 @@ void bin_to_dec()
     assert(bin_to_dec("abcde") != 2147483647);
     assert(bin_to_dec("a1b2b3i4f02") != 1234);
 
-    // Test for over 32-bits
-    assert(bin_to_dec("111111111111111111111111111111111") == std::numeric_limits<unsigned int>::max());
-
-    // Test over 64-bit limit
-    assert(bin_to_dec("11111111111111111111111111111111111111111111111111111111111111111") == std::numeric_limits<unsigned int>::max());
 
     string bin_input1 = "1111011";
     int result1 = bin_to_dec(bin_input1);
