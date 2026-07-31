66ecb4e61c8361d535f5b10eaf659b52160c0815
Kiandisor <mojynardi@gmail.com>
2026-07-31 19:49:16 +1000

    Refactored hex_to_dec
    Changed the hex_to_dec function to behave the same way as the other
    _to_dec functions in basics

--- coresdk/src/coresdk/basics.cpp
+++ coresdk/src/coresdk/basics.cpp
@@ -341,11 +341,28 @@ namespace splashkit_lib
     {
         if (!is_hex(hex_string))
         {
-            LOG(ERROR) << "Invalid octal string \"" << hex_string << "\" passed to hex_to_dec. Returning 0.";
+            LOG(ERROR) << "Invalid hex string \"" << hex_string << "\" passed to hex_to_dec. Returning 0.";
             return 0;
         }
 
-        return std::stoi(hex_string, nullptr, 16);
+        try
+        {
+	  const unsigned long result = std::stoul(hex_string, nullptr, 16);
+
+	  if (result > std::numeric_limits<unsigned int>::max())
+	  {
+	      return std::numeric_limits<unsigned int>::max();
+	  }
+	  else
+	  {
+	      return static_cast<unsigned int>(result);
+	  }
+        }
+        catch (const std::exception& error)
+        {
+            LOG(ERROR) << "Invalid hex string \"" << hex_string << "\" passed to hex_to_dec. Returning 0.";
+	  return 0;
+        }
     }
 
     string oct_to_bin(const string &octal_str)
