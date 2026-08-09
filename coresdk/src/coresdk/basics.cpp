9dc7c93314c8c63ab6a43021d15a8bceab515c02
Kiandisor <mojynardi@gmail.com>
2026-08-09 19:36:03 +1000

    Added generic conversion function
    Added a generic conversion function that will take a string and convert
    it to the type provided. Another function that clamps the value is added
    to not repeat the same code

--- coresdk/src/coresdk/basics.cpp
+++ coresdk/src/coresdk/basics.cpp
@@ -143,6 +143,40 @@ namespace splashkit_lib
         return (*p == 0);
     }
 
+    template<typename output>
+    output clamp_in_range(output number)
+    {
+        if (result > std::numeric_limits<output>::max())
+        {
+            return std::numeric_limits<output>::max();
+        }
+        else
+        {
+            return static_cast<output>(result);
+        }
+    }
+
+    template<typename output>
+    output convert_string(const string &input, int base = 10)
+    {
+        try
+        {
+            if constexpr (std::same_as<double, output>)
+            {
+                return clamp_in_range<output>(std::stod(input))
+            }
+            else
+            {
+                return clamp_in_range<output>(std::stoul(input, nullptr, base));
+            }
+        }
+        catch (const std::exception& error)
+        {
+            LOG(ERROR) << "Invalid string \"" << input << "\" passed to conversion function. Returning 0.";
+            return 0;
+        }
+    }
+
     int convert_to_integer(const string &text)
     {
         return std::stoi(text);
@@ -206,24 +240,7 @@ namespace splashkit_lib
             return 0;
         }
 
-        try
-        {
-            const unsigned long result = std::stoul(bin_str, nullptr, 2);
-
-            if (result > std::numeric_limits<unsigned int>::max())
-            {
-                return std::numeric_limits<unsigned int>::max();
-            }
-            else
-            {
-                return static_cast<unsigned int>(result);
-            }
-        }
-        catch (const std::exception& error)
-        {
-            LOG(ERROR) << "Invalid binary string \"" << bin_str << "\" passed to bin_to_dec. Returning 0.";
-            return 0;
-        }
+        return convert_string<unsigned int>(bin_str, 2);
     }
 
     string hex_to_bin(const string &hex_str)
@@ -317,24 +334,7 @@ namespace splashkit_lib
             return 0;
         }
 
-        try
-        {
-            const unsigned long result = std::stoul(octal_string, nullptr, 8);
-
-            if (result > std::numeric_limits<unsigned int>::max())
-            {
-                return std::numeric_limits<unsigned int>::max();
-            }
-            else
-            {
-                return static_cast<unsigned int>(result);
-            }
-        }
-        catch (const std::exception& error)
-        {
-            LOG(ERROR) << "Invalid octal string \"" << octal_string << "\" passed to oct_to_dec. Returning 0.";
-            return 0;
-        }
+        return convert_string<unsigned int>(octal_string, 8);
     }
 
     unsigned int hex_to_dec(const string &hex_string)
@@ -345,24 +345,7 @@ namespace splashkit_lib
             return 0;
         }
 
-        try
-        {
-            const unsigned long result = std::stoul(hex_string, nullptr, 16);
-
-            if (result > std::numeric_limits<unsigned int>::max())
-            {
-                return std::numeric_limits<unsigned int>::max();
-            }
-            else
-            {
-                return static_cast<unsigned int>(result);
-            }
-        }
-        catch (const std::exception& error)
-        {
-            LOG(ERROR) << "Invalid hex string \"" << hex_string << "\" passed to hex_to_dec. Returning 0.";
-            return 0;
-        }
+        return convert_string<unsigned int>(hex_string, 16);
     }
 
     string oct_to_bin(const string &octal_str)
