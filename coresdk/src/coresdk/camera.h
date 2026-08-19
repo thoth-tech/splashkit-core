2365763efefac9627680da15a829920f85a3a9d5
Michael N <59274326+olive-tree-branch@users.noreply.github.com>
2026-08-19 02:14:54 +0000

    Changed documentation
    Changed the wording in the header documentation to be more clear what the outcome is

--- coresdk/src/coresdk/camera.h
+++ coresdk/src/coresdk/camera.h
@@ -90,7 +90,7 @@ namespace splashkit_lib
     point_2d screen_center();
 
     /**
-     * Set the camera back to the centre position on the screen.
+     * Reset the camera position back to (x = 0, y = 0).
      *
      * This is the equivilent to calling the functions set_camera_x(0) and set_camera_y(0).
      */
